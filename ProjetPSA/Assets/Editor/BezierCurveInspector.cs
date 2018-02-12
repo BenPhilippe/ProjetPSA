b109c89ad96812a75b23c6edc5a50f4798458ca7
Baj <benjphilippe@hotmail.fr>
2018-02-12 16:09:49 +0100

    Bezier optimization

--- ProjetPSA/Assets/Editor/BezierCurveInspector.cs
+++ ProjetPSA/Assets/Editor/BezierCurveInspector.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 [CustomEditor(typeof(BezierCurve))]
 public class BezierCurveInspector : Editor {
+
+	private const int lineSteps = 10;
+
 	private BezierCurve curve;
 	private Transform handleTransform;
 	private Quaternion handleRotation;
@@ -17,9 +20,19 @@ public class BezierCurveInspector : Editor {
 		Vector3 p1 = ShowPoint(1);
 		Vector3 p2 = ShowPoint(2);
 
-		Handles.color = Color.white;
+		//Draw lines between each point
+		Handles.color = Color.gray;
 		Handles.DrawLine(p0, p1);
 		Handles.DrawLine(p1, p2);
+
+		//Draw curves
+		Handles.color = Color.white;
+		Vector3 lineStart = curve.GetPoint(0f);
+		for (int i = 1; i < lineSteps; i++){
+			Vector3 lineEnd = curve.GetPoint(i/(float)lineSteps);
+			Handles.DrawLine(lineStart, lineEnd);
+			lineStart = lineEnd;
+		}
 	}
 
 	private Vector3 ShowPoint(int index){
---
+++ ProjetPSA/Assets/PSA_Assets/Scripts/Bezier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public static class Bezier {
+	public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t){
+		//line from start to end
+		//return Vector3.Lerp(p0, p2, t);
+
+		//Three calls to Lerp()
+		//return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+
+		//Quadratic Bezier curve formula
+		//B(t) = (1 - t)² P0 + 2 (1 - t) t P1 + t² P2
+		t = Mathf.Clamp01(t);
+		float invertT = 1f - t;
+		return 
+			invertT * invertT * p0 +
+			2f * invertT * t * p1 +
+			t * t * p2;
+	}
+
+	public static Vector3 GetFirstDerivative (Vector3 p0, Vector3 p1, Vector3 p2, float t){
+		return
+			2f * (1f - t) * (p1 - p0) +
+			2f * t * (p2 - p1);
+	}
+}
