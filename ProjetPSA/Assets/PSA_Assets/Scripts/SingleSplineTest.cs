4407d69c8d9a75810c35d875b8f41d622baebd0e
Baj <benjphilippe@hotmail.fr>
2018-02-12 14:32:35 +0100

    Line + inspector
    line between two points
    inspector moving, rotating and undoing

---
+++ ProjetPSA/Assets/PSA_Assets/Scripts/SingleSplineTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleSplineTest : MonoBehaviour {
+
+	public Transform[] points;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
